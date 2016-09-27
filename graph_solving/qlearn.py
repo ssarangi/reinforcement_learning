7396bb4876d2bdcce24fa45432b2006a422e6b9d
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-09-27 00:48:30 -0700

    First version working.. added a pickle file for a case which used to hang.. right now passes by fluke since Q matrix is not initialized properly

--- graph_solving/qlearn.py
+++ graph_solving/qlearn.py
@@ -1,10 +1,16 @@
 class QLearner:
-    def __init__(self, model_size, start, goal, gamma):
+    def __init__(self, model_size, gamma):
         size = model_size
-        self.Q = [[0] * (size-1) for _ in range(0, size)]
-        self.current_state = start
-        self.goal = goal
+        self.Q = [[0] * size for _ in range(0, size)]
         self.gamma = gamma
+        self.start = -1
+        self.goal = -1
+        self.current_state = -1
+
+    def set_start_and_goal(self, start, goal):
+        self.start = start
+        self.goal = goal
+        self.current_state = start
 
     def update_current_state(self, state):
         self.current_state = state
@@ -17,3 +23,9 @@ class QLearner:
 
         self.Q[self.current_state][next_state] = reward_matrix[self.current_state][next_state] + self.gamma * next_state_action_Q
         self.current_state = next_state
+
+    def stop_training(self):
+        self.gamma = 1.0
+
+    def get_Q_value(self, current_state, next_state):
+        return self.Q[current_state][next_state]
--- graph_solving/qlearnsolver.py
+++ graph_solving/qlearnsolver.py
@@ -1,6 +1,9 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
+import pickle
+import os
+import sys
 
 from qlearn import QLearner
 
@@ -22,38 +25,94 @@ class QLearnSolver:
         for i in range(0, num_nodes):
             n1 = random.randint(0, num_nodes - 1)
             n2 = random.randint(0, num_nodes - 1)
-            if n1 == n2:
+            if n1 == n2 or n1 in self.graph.neighbors(n2):
                 continue
             self.graph.add_edge(n1, n2)
 
         # Make sure there is atleast one edge between the start and goal
+        if start in self.graph.neighbors(goal):
+            self.graph.remove_edge(goal, start)
+
         self.graph.add_edge(start, goal)
 
-        # Add one more with a node in between
-        b1 = random.randint(0, num_nodes - 1)
-        self.graph.add_edge(start, b1)
-        self.graph.add_edge(b1, goal)
-
         # Create reward matrix
-        self.reward = [[0] * num_nodes for _ in range(0, num_nodes)]
+        self.reward_matrix = [[0] * (num_nodes) for _ in range(0, num_nodes)]
 
         goal_reward = 100
         for u, v in self.graph.edges():
             if v == goal:
-                self.reward[u][v] = goal_reward
-
-    def get_reward_matrix(self):
-        return self.reward
+                self.reward_matrix[u][v] = goal_reward
 
     def get_model_size(self):
         return self.num_nodes - 1
 
     def execute_training(self):
+        self.qlearner.set_start_and_goal(self.start, self.goal)
+        visited = {self.start}
         while self.qlearner.current_state != self.qlearner.goal:
             # From the current state figure out what are the possible next actions and select a random one
-            actions = []
+            actions = self.graph.neighbors(self.qlearner.current_state)
 
-            self.qlearner.execute_step()
+            # Check if all the actions have been visited or not.. If so then we have a cycle in
+            # the graph
+            all_actions_visited = True
+            for action in actions:
+                if action not in visited:
+                    all_actions_visited = False
+
+            if all_actions_visited:
+                return
+
+            if len(actions) == 0:
+                return
+
+            max_idx = len(actions)
+            next_state = actions[random.randint(0, max_idx - 1)]
+            if next_state in visited:
+                continue
+            else:
+                visited.add(next_state)
+
+            next_state_actions = self.graph.neighbors(next_state)
+            self.qlearner.execute_step(next_state, next_state_actions, self.reward_matrix)
+
+    def execute(self, trace=False):
+        if trace:
+            print("-" * 10 + " Execution tracing " + "-" * 10)
+            print("Next State: %s" % self.start)
+        self.qlearner.set_start_and_goal(self.start, self.goal)
+        self.qlearner.stop_training()
+        path = [self.qlearner.current_state]
+
+        visited = {self.qlearner.current_state}
+
+        while self.qlearner.current_state != self.qlearner.goal:
+            actions = self.graph.neighbors(self.qlearner.current_state)
+
+            if len(actions) == 0:
+                return path
+
+            next_state = actions[0]
+            max_v = 0
+            for action in actions:
+                if action in visited:
+                    continue
+                visited.add(action)
+                curr_v = self.qlearner.get_Q_value(self.qlearner.current_state, action)
+                if curr_v > max_v:
+                    next_state = action
+                    max_v = curr_v
+
+            if trace:
+                print("Next State: %s Q Val: %s" % (next_state, max_v))
+
+            path.append(next_state)
+            self.qlearner.current_state = next_state
+
+        if trace:
+            print("-" * 10 + " Done Execution " + "-" * 10)
+
+        return path
 
     def render(self):
         nodes = [i for i in range(0, self.num_nodes)]
@@ -87,19 +146,65 @@ class QLearnSolver:
 
         nx.draw_networkx_labels(self.graph, pos, labels, font_size=16)
         plt.show()
+        print("Rendering")
+
+def create_new_qlearn_solver(model_size, qlearner):
+    start = random.randint(0, model_size - 1)
+    goal = start
+    while goal == start:
+        goal = random.randint(0, model_size - 1)
+
+    qlearnsolver = QLearnSolver(model_size, start, goal, qlearner)
+    qlearner.set_start_and_goal(start, goal)
+    return qlearnsolver
+
 
 def main():
-    num_iterations = 100
-    model_size = 10
-    qlearner = QLearner(model_size)
-    for i in range(0, num_iterations):
-        start = random.randint(0, model_size - 1)
-        goal = start
-        while goal == start:
-            goal = random.randint(0, model_size - 1)
+    existing_file = ""
 
-        qlearnsolver = QLearnSolver(model_size, start, goal, qlearner)
-        qlearnsolver.render()
+    if len(sys.argv) > 1:
+        existing_file = sys.argv[1]
+
+    num_execution_iterations = 1
+    if existing_file == "":
+        num_execution_iterations = 2000
+
+    for exec_iter in range(0, num_execution_iterations):
+        print("Execution Iteration: %s" % exec_iter)
+        print("-" * 50)
+        num_training_iterations = 10000
+        model_size = 10
+        qlearner = QLearner(model_size, 0.3)
+
+        if existing_file == "":
+            qlearnsolver = create_new_qlearn_solver(model_size, qlearner)
+            with open('qlearnsolver.pkl', 'wb') as output:
+                pickle.dump(qlearnsolver, output, pickle.HIGHEST_PROTOCOL)
+        else:
+            with open(existing_file, 'rb') as input:
+                qlearnsolver = pickle.load(input)
+                qlearnsolver.render()
+
+        for i in range(0, num_training_iterations):
+            qlearner.set_start_and_goal(qlearnsolver.start, qlearnsolver.goal)
+            qlearnsolver.execute_training()
+
+        print("Completed Training")
+        path = qlearnsolver.execute()
+        print("Start: %s End: %s" % (qlearnsolver.start, qlearnsolver.goal))
+        print("Found path: %s" % path)
+        if path[0] != qlearnsolver.start or path[-1] != qlearnsolver.goal or len(path) > 2:
+            print("Error in predicting....")
+            print("Q = ")
+            for i in range(0, model_size):
+                print(" ".join(str(j) for j in qlearner.Q[i]))
+            qlearnsolver.render()
+            path = qlearnsolver.execute(trace=True)
+
+        if existing_file == "":
+            os.remove("qlearnsolver.pkl")
+
+        print("-" * 50)
 
 if __name__ == "__main__":
     main()
