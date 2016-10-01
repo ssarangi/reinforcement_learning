e5530b5a0eb3c3fe1fb64132b53502ebd46cf31f
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-09-30 22:27:33 -0700

    added q learning

--- flappy_bird/flappybird.py
+++ flappy_bird/flappybird.py
@@ -4,6 +4,12 @@ import pygame
 from pygame.locals import *  # noqa
 import sys
 import random
+from enum import Enum
+
+
+class Actions(Enum):
+    CLICK = 0,
+    DO_NOTHING = 1
 
 
 class FlappyBird:
@@ -30,6 +36,9 @@ class FlappyBird:
         self.debug=True
         self.alpha = alpha
         self.gamma = gamma
+        self.horizontal_dist_dash = 99999
+        self.vertical_dist_hash = 99999
+        self.action_to_perform = random.choice([Actions.CLICK, Actions.DO_NOTHING])
 
     def updateWalls(self):
         self.wallx -= 2
@@ -37,6 +46,9 @@ class FlappyBird:
             self.wallx = 400
             self.counter += 1
             self.offset = random.randint(-110, 110)
+
+    def getQ(self, state):
+        return self.q.get(state, 0.0)
 
     def birdUpdate(self):
         if self.jump:
@@ -81,31 +93,48 @@ class FlappyBird:
             pygame.draw.lines(self.screen, border_color, False, [(self.bird[0] + self.bird[2], self.bird[1] + self.bird[3]),
                                                                  (self.bird[0] + self.bird[2], 360 + self.gap - self.offset + 10)], 2)
 
-            horizontal_dist = self.wallx - (self.bird[0] + self.bird[2])
-            vertical_dist   = (360 + self.gap - self.offset + 10) - (self.bird[1] + self.bird[3])
+            horizontal_dist = min(0, self.wallx - (self.bird[0] + self.bird[2]))
+            vertical_dist = min(0, (360 + self.gap - self.offset + 10) - (self.bird[1] + self.bird[3]))
+
+            print("Horizontal Dist: %s --- Vertical Dist: %s" % (horizontal_dist, vertical_dist))
+            input()
 
             # Q[s,a] ← Q[s,a] + α (r + γ * V(s') - Q[s,a])
-            current_state = (horizontal_dist, vertical_dist, self.dead)
-            reward = 1 if self.dead == False else -1000
+            v_dash_do_nothing = self.getQ((self.horizontal_dist_dash, self.vertical_dist_hash, Actions.DO_NOTHING))
+            v_dash_click = self.getQ((self.horizontal_dist_dash, self.vertical_dist_hash, Actions.CLICK))
+            v_dash = max(v_dash_click, v_dash_do_nothing)
 
-            original = self.q.get(current_state, 1.0)
-            self.q[current_state] = original + alpha * ()
+            current_state = (horizontal_dist, vertical_dist, self.action_to_perform)
+            reward = 1 if self.dead is False else -1000
+
+            original = self.getQ(current_state)
+            self.q[current_state] = original + self.alpha * (reward + self.gamma * v_dash - original)
+            print("Current State: %s   --> Q Value: %s" % (current_state, self.q[current_state]))
 
             pygame.draw.rect(self.screen, blue_color, self.bird, 2)
             pygame.draw.rect(self.screen, border_color, upRect, 2)
             pygame.draw.rect(self.screen, border_color, downRect, 2)
             self.screen.blit(self.birdSprites[self.sprite], (70, self.birdY))
 
+            click_action = self.getQ((horizontal_dist, vertical_dist, Actions.CLICK))
+            do_nothing_action = self.getQ((horizontal_dist, vertical_dist, Actions.DO_NOTHING))
+
+            if click_action > do_nothing_action:
+                return Actions.CLICK
+            else:
+                return Actions.DO_NOTHING
+
     def run(self):
         clock = pygame.time.Clock()
         pygame.font.init()
         font = pygame.font.SysFont("Arial", 50)
+        next_action = random.choice([Actions.CLICK, Actions.DO_NOTHING])
         while True:
             clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                if (event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN) and not self.dead:
+                if (event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN or next_action == Actions.CLICK) and not self.dead:
                     self.jump = 17
                     self.gravity = 5
                     self.jumpSpeed = 10
@@ -127,7 +156,10 @@ class FlappyBird:
             if not self.dead:
                 self.sprite = 0
             self.updateWalls()
-            self.birdUpdate()
+            next_action = self.birdUpdate()
+            for k, v in self.q.items():
+                print(k, v)
+
             pygame.display.update()
 
 if __name__ == "__main__":
